f6e3ae64585a46d0567484b59003db50512995bb
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-08 11:03:39 +0800

    Run on startup

--- firefox.bat
+++ firefox.bat
@@ -1,5 +1,9 @@
 @echo off
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" google.com youtube.com
-echo Firefox has started. Google has started. Youtube has started.
-pause
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe" mail.yahoo.com outlook.office.com/mail/inbox github.com/NotTheRealEdmund
 EXIT /B
+
+REM This is a comment. 
+REM This batch file will be used to run on windows startup automatically, so we'll remove pause, there's no need for user-verification. 
+REM We'll also remove the echo because the cmd will close before we can even look at the message.
+REM Batch files will automatically close once done, although it's better to play safe and use EXIT /B to make sure it isn't secretly
+REM running in the background without us knowing.

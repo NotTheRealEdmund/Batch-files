6501a5f9855966ff99198640b6239503a5bd44fe
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-08 10:45:29 +0800

    Open urls in firefox

--- firefox.bat
+++ firefox.bat
@@ -1,5 +1,5 @@
 @echo off
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
-echo Firefox has started.
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe" google.com youtube.com
+echo Firefox has started. Google has started. Youtube has started.
 pause
 EXIT /B
